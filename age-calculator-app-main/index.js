14330e534e779ab3309184b40212b988fc50fbdf
L-V-Rushyendra <20bq1a05c1.vvit@gmail.com>
2023-05-18 14:53:37 +0530

    no of days task added

---
+++ age-calculator-app-main/index.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".calcform");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const day = document.querySelector("#day").value
+    const month = document.querySelector("#month").value
+    const year = document.querySelector("#year").value
+
+})
